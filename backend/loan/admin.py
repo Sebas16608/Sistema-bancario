08226f0a321ab4405c4102ebeb79f17d1c18236a
Sebas16608 <asebasrr444@gmail.com>
2025-08-31 12:01:47 -0600

    registrando los modelos de Loan al admin

--- backend/loan/admin.py
+++ backend/loan/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Credito, Pago, Ahorro, Deposito
 # Register your models here.
+admin.site.register(Credito)
+admin.site.register(Pago)
+admin.site.register(Ahorro)
+admin.site.register(Deposito)
