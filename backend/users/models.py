b0fcf5c79edf5e8f61b7c34a777f0333ebb80f9f
Sebas16608 <asebasrr444@gmail.com>
2025-09-01 09:46:23 -0600

    Arreglando el modelo de users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+
 class Usuario(AbstractUser):
     nombre = models.CharField(max_length=100)
     ROLES = [
-        ("ADMINISTRADOR", "Administrador")
-        ("CAJERO", "Cajero")
+        ("JEFE", "Jefe"),
+        ("CAJERO", "Cajero"),
     ]
-    rol = models.CharField(max_length=20, choices=ROLES, default="Cajero")
+    rol = models.CharField(max_length=20, choices=ROLES, default="CAJERO")  # ojo que default sea consistente
