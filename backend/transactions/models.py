5bf83f24063deba1d1729d0e228a2613c9b6d397
Sebas16608 <asebasrr444@gmail.com>
2025-09-01 09:20:04 -0600

    Creando los modelos de transactions

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from clientes.models import Cliente
+from loan.models import Pago, Deposito
 # Create your models here.
+class Cuenta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=255)
+    pago = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Cuenta"
+        verbose_name_plural = "Cuentas"
+
+    def __str__(self):
+        return f"El pago de {self.pago} fue exitoso en la cuenta de {self.cliente}"
+    
+class Movimiento(models.Model):
+    cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo = models.CharField(max_length=255)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Movimiento"
+        verbose_name_plural = "Movimientos"
+    
+    def __str__(self):
+        return f"movimiento de Q{self.monto} realizado correctamente"
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class Usuario(AbstractUser):
+    nombre = models.CharField(max_length=100)
+    ROLES = [
+        ("ADMINISTRADOR", "Administrador")
+        ("CAJERO", "Cajero")
+    ]
+    rol = models.CharField(max_length=20, choices=ROLES, default="Cajero")
