61d6bca683e4e57863d9182dd8c91b0b2a349a7e
Sebas16608 <asebasrr444@gmail.com>
2025-09-01 16:29:20 -0600

    Creando el metodo GET para la API de Clientes

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ClienteSerializer, ContactoSerializer, DocumentsSerliarizer
 from .models import Cliente, Contacto, Documents
-# Create your views here.
+
+class ClienteView(APIView):
+    # Metodo Get
+    def get_cliente(self, request, pk=None):
+        if pk:
+            try:
+                cliente = Cliente.objects.get(pk=pk)
+                serializer = ClienteSerializer(cliente)
+                return Response(serializer.data)
+            except Cliente.DoesNotExist:
+                return Response({"error": "Cliente no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            cliente = Cliente.objects.all()
+            serializer = ClienteSerializer(cliente, many=True)
+            return Response(serializer.data)
