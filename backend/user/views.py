3140a031109a57abf3eee713f9ff1932238c4110
Sebas16608 <asebasrr444@gmail.com>
2025-09-05 08:26:59 -0600

    Creando API de Users, ya esta terminada

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIview
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from .models import User
 # Create your views here.
+class UserView(APIview):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                users = User.objects.get(pk=pk)
+                serializer = UserSerializer(users, many=True)
+                return Response(serializers.data)
+            except User.DoesNotExist:
+                return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            users = User.objects.all()
+            serializer = UserSerializer(users, many=True)
+            return Response(serializer.data)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        serializer = UserSerializer
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        try:
+            users = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(users, data=request.data)
+        if serializers.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try: 
+            users = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+        users.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
