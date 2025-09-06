21bbedf0222d31f3e48ef1e22fad6544372d0cc9
Sebas16608 <asebasrr444@gmail.com>
2025-09-06 09:42:52 -0600

    Settings, .env

--- backend/sistema/settings.py
+++ backend/sistema/settings.py
@@ -13,17 +13,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolvez().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$-na*!1o(%!zsdgzva%irqgdupwd-d2f4p$w84%m9vqtraghm7'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -56,6 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'sistema.urls'
+
+AUTH_USER_MODEL = config("AUTH_USER_MODEL")
 
 TEMPLATES = [
     {
